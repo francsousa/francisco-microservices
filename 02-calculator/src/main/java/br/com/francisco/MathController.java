a07cd3289848d5fb19e4ca92d27ea942d9b2c96d
francisco <francisco@pop-os.localdomain>
2021-06-20 11:53:57 -0300

    Os métodos de validação

--- 02-calculator/src/main/java/br/com/francisco/MathController.java
+++ 02-calculator/src/main/java/br/com/francisco/MathController.java
@@ -17,13 +17,16 @@ public class MathController {
 		return sum;
 	}
 
-	private Double convertToDouble(String numberTwo) {
-		// TODO Auto-generated method stub
-		return 1D;
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
 	}
 
-	private boolean isNumeric(String number) {
-		// TODO Auto-generated method stub
-		return false;
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
 	}
 }
